d2f7b37b382f5d650516df95faf74a316a37f016
Nab69 <17692533+Nab69@users.noreply.github.com>
2022-01-18 11:37:43 +0100

    SchoolApi project added

---
+++ SchoolApi/Controllers/ClassroomController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClassroomController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetClassrooms()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetClassroom(int id)
+        {
+            if(id <= 0)
+                return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult PostClassroom()
+        {
+            return Created("", null);
+        }
+
+        [HttpPut]
+        public IActionResult PutClassroom()
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteClassroom(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
