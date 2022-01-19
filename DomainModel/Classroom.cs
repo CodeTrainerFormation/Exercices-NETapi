897a705cc5f36ea9ff6980c95a646e980d85aae3
Nab69 <17692533+Nab69@users.noreply.github.com>
2022-01-19 10:15:06 +0100

    entities and attributes

--- DomainModel/Classroom.cs
+++ DomainModel/Classroom.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DomainModel
 {
-    [Table("Classroom")]
+    //[Table("Classroom")]
     public class Classroom
     {
         //[Key]
+        //[Column("id")]
         public int ClassroomID { get; set; }
         public string Name { get; set; }
         public int Floor { get; set; }
         public string Corridor { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+        public virtual Teacher Teacher { get; set; }
     }
 }
